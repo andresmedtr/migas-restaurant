7d01bce51706df03203701ec35f6d1097783846d
andresmedtr <123752360+andresmedtr@users.noreply.github.com>
2024-02-19 19:08:19 -0500

    editted the webpack config

--- next.config.js
+++ next.config.js
@@ -5,9 +5,11 @@ const nextConfig = {
   },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
+    // config.experimental.webpackBuildWorker = true;
 
     return config;
   },
+  experimental: { webpackBuildWorker: true },
 };
 
 module.exports = nextConfig;
