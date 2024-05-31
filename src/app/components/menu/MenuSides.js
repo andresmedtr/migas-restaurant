04192f78b3d24a4f1d0ee72e3b01cb39c0fa74f0
andresmedtr <123752360+andresmedtr@users.noreply.github.com>
2024-05-31 11:53:26 -0400

    modified colors on sidesbutton and UX

--- src/app/components/menu/MenuSides.js
+++ src/app/components/menu/MenuSides.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import Draggable from "react-draggable";
 import "@/styles/menusides.css";
 import { motion } from "framer-motion";
 import Image from "next/image";
@@ -13,15 +14,10 @@ const MenuSides = () => {
 
   return (
     <div className="">
-      <div className="d-flex justify-content-end fixedButton me-4">
+      <div className="d-flex justify-content-end fixedButton">
         <button
           onClick={handleSides}
-          className={`btn ${showSides && "d-none"}`}
-          style={{
-            width: "80%",
-            backgroundColor: "#FFA50080",
-            color: "#000000",
-          }}>
+          className={`btn sides-button ${showSides && "d-none"}`}>
           <p className="m-auto">
             S
             <br />
@@ -40,21 +36,24 @@ const MenuSides = () => {
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: -10 }}
           transition={{ duration: 1 }}>
-          <div className="sides-container">
-            <FontAwesomeIcon
-              icon={faTimes}
-              onClick={handleSides}
-              className=" align-items-center closeButton text-light"
-            />
-            <Image
-              src="/assets/sides.png"
-              className="sidesImage"
-              width={250}
-              height={250}
-              alt="Sides"
-              key="image"
-            />
-          </div>
+          <Draggable>
+            <div className="sides-container">
+              <FontAwesomeIcon
+                icon={faTimes}
+                onClick={handleSides}
+                className="align-items-center closeButton text-light"
+              />
+
+              <Image
+                src="/assets/sides.png"
+                className="sidesImage"
+                width={250}
+                height={250}
+                alt="Sides"
+                key="image"
+              />
+            </div>
+          </Draggable>
         </motion.div>
       )}
     </div>
